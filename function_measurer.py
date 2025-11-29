9bc3b5b310e210ec958704e2c567ad80705c16e4
SpatialPoet1950 <lucas.satiadonog@gmail.com>
2025-11-29 14:23:22 -0700

    Update function_measurer.py

--- function_measurer.py
+++ function_measurer.py
@@ -3,7 +3,7 @@ class function:
         if arr is not None:
             self.x = arr
         else: 
-            x = int(input("Grade of the equation: "))
+            x = int(input("Degree of the function(for example ax^3+bx^2+c will be degree 3): "))
             self.x = []
             for i in range (x+1):
                 if i == (x):        value = float(input(f'Value of the constant: '))
@@ -48,13 +48,14 @@ class function:
 
             while round(self.plot(valor), 6)!=0:
                 valor -= self.plot(valor)/self.derivative(valor)
-                if n>1001:
-                    irreal = True
+                if n>100:
+                    imaginary = True
                     break
                 n+=1
-            if irreal is True:
-                for i in range(20):
-                    valor -= self.plot(valor*1j)/self.derivative(valor)
+            if imaginary is True:
+                valor*= 1j
+                for i in range(30):
+                    valor -= self.plot(valor)/self.derivative(valor)
             try:
                 roots.append(round(valor, 5))
             except:
@@ -74,19 +75,25 @@ class function:
     
     def mean_value(self, x0=0, x1=1):
         return (self.derivative(x0)-self.derivative(x1))/(x0-x1)
-tipo = int(input("What do you want to do? \n1. - Plot the function's value \n2. - Get the function's derivative \n3 - Get function's area(Integral)\n4. - Get function's roots\n>>: "))
-if tipo==1:
+typo = int(input("What do you want to do? \n1. - Plot the function's value \n2. - Get the function's derivative \n3. - Get function's area(Integral)\n4. - Get function's roots\n>>: "))
+if typo==1:
   number = float(input("What is your selected value?"))
   print(function().plot(value=number))
-elif tipo == 4:
+elif typo == 4:
   print(function().roots())
-elif tipo == 2:
+elif typo == 2:
     number = input("If you want to select a value, digit it, if no, press Enter: ")
     try:
         number = float(number)
         print(function().derivative(value=number))
-    except:   
-        print(function().derivative())
+    except:
+        result = function().derivative()[::-1]
+        result2 = ""
+        for i in range(len(result)):
+            if i==0: result2 =  result2+str(result[i])
+            else: result2 = str(result[i])+f"x^{i} + "+result2
+        print(result2)
+
 else:
   up = input("If you want to select the up part of the integral, digit, if no, Enter:")
   if up is None: print(function().integral())
